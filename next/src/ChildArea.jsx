5246058ab72839c0ba1f8f8aae5c938472d23075
haru-oss <hauyaishungry@gmail.com>
2024-04-28 17:46:54 +0900

    memoを使用して、親の影響で子に

--- next/src/ChildArea.jsx
+++ next/src/ChildArea.jsx
@@ -1,3 +1,5 @@
+import {memo} from "react"
+
 const style = {
     width:"100%",
     height:"200px",
@@ -5,7 +7,7 @@ const style = {
 }
 
 
-export const ChildArea = (props) => {
+export const ChildArea = memo((props) => {
     const {open} = props;
     console.log("Childareaがレンダリングされた");
 
@@ -34,4 +36,4 @@ export const ChildArea = (props) => {
 
 
 
-};
+});
