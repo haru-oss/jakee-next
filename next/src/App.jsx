65a55ea33be71cdb4dc74f877cad8509d92e5986
haru-oss <hauyaishungry@gmail.com>
2024-04-30 18:13:38 +0900

    react-router-domはアップデートされた影響で、udemyの記述を真似してもその通りには動かなくなっていた。

--- next/src/App.jsx
+++ next/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import {BrowserRouter,Link} from "react-router-dom"
+import {BrowserRouter,Link,Route,Routes} from "react-router-dom"
 
 // import {useState,useCallback} from 'react'
 
@@ -12,15 +12,32 @@ export const App=()=> {
      return (
        <BrowserRouter>
           <div>
-            <Link to="/">home</Link>
+            <Link to="/">Home</Link>
             <br />
-            <Link to="page1">page1</Link>
+            <Link to="page1">Page1</Link>
             <br />
-            <Link to="page2">page2</Link>
-            <Home/>
-            <Page1/>
-            <Page2/>
+            <Link to="page2">Page2</Link>
           </div>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="page1" element={<Page1 />} />
+            <Route path="page2" element={<Page2 />} />
+
+
+            {/* <Route path='/'> */}
+{/*
+            </Route>
+            <Route path='/page1'>
+              <Page1/>
+            </Route>
+            <Route path='/page2'>
+              <Page2/>
+     </Route> */}
+          </Routes>
+
+
+
 
       </BrowserRouter>
 
