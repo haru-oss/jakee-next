b4afe85257d2343577c77adf84e3fb2d48a0f393
haru-oss <hauyaishungry@gmail.com>
2024-05-02 15:24:35 +0900

    ルートの入れ子の設定をしていたところ、躓いた

--- next/src/Page1.jsx
+++ next/src/Page1.jsx
@@ -1,7 +1,23 @@
+import {Link} from "react-router-dom"
+import { Page1DetailA } from "./Page1DetailA";
+import {Page1DetailB}  from "./Page1DetailB"
+
 export const Page1 = () => {
     return(
-        <div>
-            <h1>Page1</h1>
-        </div>
+
+        // <BrowserRouter>
+          <div>
+              <h1>Page1</h1>
+              <Link to = "/page1/detailA">DetaileA</Link>
+              <br />
+              <Link to = "/page1/detailB">DetaileB</Link>
+          </div>
+
+        //   <Routes>
+        //       <Route path="/page1/detailA" element={<Page1DetailA />} />
+        //       <Route path="/page1/detailB" element={<Page1DetailB />} />
+        //   </Routes>
+
+        /* </BrowserRouter> */
     )
 };
---
+++ next/src/Page1DetailB.jsx
@@ -0,0 +1,9 @@
+export const Page1DetailB= () => {
+    return(
+
+
+        <div>
+            <h1>Page1DetailB</h1>
+        </div>
+    )
+};
